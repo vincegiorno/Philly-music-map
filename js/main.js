6f8608927bf4e3ca4f77b77c3fbf8187231c7b1e
vinceg <vincegiorno324@gmail.com>
2015-04-23 12:38:30 -0400

    add data & image files

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
 var mapView = function() {
 
-var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(39.9749224,-75.2391609),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+  var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(39.9749224,-75.2391609),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  var map = new google.maps.Map(document.getElementsByClassName("map-canvas")[0], mapOptions);
 
-// this is our gem
-google.maps.event.addDomListener(window, "resize", function() {
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-});
+  // this is our gem
+  google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center);
+  });
 }
