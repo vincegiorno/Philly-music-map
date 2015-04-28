ba5fcd53d980c04320a2545305b0381c517f923e
vinceg <vincegiorno324@gmail.com>
2015-04-27 23:46:46 -0400

    bring Venue constructor into vm

--- js/main.js
+++ js/main.js
@@ -1,18 +1,119 @@
-
 var mapView = function() {
 
   var mapOptions = {
-      zoom: 12,
-      center: new google.maps.LatLng(39.9749224,-75.2391609),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 12,
+    center: new google.maps.LatLng(39.9749224, -75.2391609),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   var map = new google.maps.Map(document.getElementsByClassName("map-canvas")[0], mapOptions);
 
-  // this is our gem
   google.maps.event.addDomListener(window, "resize", function() {
-      var center = map.getCenter();
-      google.maps.event.trigger(map, "resize");
-      map.setCenter(center);
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
   });
+};
+
+var image = "resources/music_live.png";
+
+/* Each venue object contains stored data and a marker as its properties */
+
+
+var vm = {};
+
+vm.searchStr = ko.observable("");
+
+vm.Venue = function(index) {
+  self = this;
+  self.id = data[index].id;
+  self.events = ko.observableArray();
+  vm.loadEvents(self.id);
+  self.name = data[index].name;
+  self.address = data[index].address;
+  self.marker = new google.maps.Marker({
+    position: {
+      lat: data[index].latitude,
+      lng: data[index].longitude
+    },
+    title: self.name,
+    icon: data.image,
+    animation: null,
+    map: null
+  });
+  google.maps.event.addListener(self.marker, 'click', view.openEventsWindow(self.events));
+  self.isVisible = ko.computed(function() {
+      if (self.name.indexOf(vm.searchStr)) >= 0) {
+      return true;
+    } else {
+      for (var i = self.events.length - 1; i >= 0; i--) {
+        if (self.events[i].date.indexOf(vm.searchStr) >= 0) {
+          return true;
+        }
+      }
+    } else {
+      for (var i = self.events.length - 1; i >= 0; i--) {
+        for (var j = self.events[i].artists - 1; j >= 0; j--) {
+          if (self.events[i].artists[j].indexOf(vm.searchStr) >= 0) {
+            return true;
+          }
+        }
+      }
+    } else {
+      return false;
+    }
+  });
+
+vm.venues = ko.observableArray();
+
+/* Function populates venues with venue objects. it is set up to execute
+twice a second because each */
+vm.initialize = function() {
+  var i = data.length - 1;
+  venues[i] = new Venue(data[i]);
+  venues[i].visible = ko.observable(true);
+  vm.getEvents(i, data[i].id);
+  i -= 1;
+  while (i >= 0) {
+    setTimeout(function() {
+      venues[i] = vm.createVenue(i);
+      i -= 1;
+    }, 500);
+  }
 }
+
+vm.getEvents = function(index, id) {
+  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    httpRequest = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE
+    try {
+      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {}
+    }
+  }
+
+  if (!httpRequest) {
+    alert('Data could not be requested from the server.');
+    return false;
+  }
+  httpRequest.onreadystatechange = processEvents(index);
+  url = "http://api.jambase.com/events?venueId=" + id +
+    "&page=0&api_key=73ntgrhffzwqdcaan4empnrd";
+  httpRequest.open('GET', url);
+  httpRequest.send();
+}
+
+function processEvents(index) {
+  if (httpRequest.readyState === 4) {
+    if (httpRequest.status === 200) {
+      venues[index].events(JSON.parse(httpRequest.responseText));
+    } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+
+view.format(data.getEvents(self.id));
---
+++ resources/place-data.js
@@ -0,0 +1,121 @@
+var data = [
+{
+"name": "Theatre of Living Arts (TLA)",
+"id": 1000,
+"address": "334 South St",
+"latitude": 39.941515,
+"longitude": -75.148335
+},
+{
+"name": "Kimmel Center",
+"id": 16004,
+"address": "300 S Broad St",
+"latitude": 39.946999,
+"longitude": -75.164845
+},
+{
+"name": "Ardmore Music Hall",
+"id": 139974,
+"address": "23 E Lancaster Ave, Ardmore",
+"latitude": 40.007595,
+"longitude": -75.290002
+},
+{
+"name": "Lincoln Financial Field",
+"id": 3461,
+"address": "1 Lincoln Financial Field Way",
+"latitude": 39.90073,
+"longitude": -75.16754
+},
+{
+"name": "Wells Fargo Center",
+"id": 259,
+"address": "3601 S Broad St",
+"latitude": 39.947,
+"longitude": -75.1648
+},
+{
+"name": "Tower Theater",
+"id": 270,
+"address": "69th St & Ludlow",
+"latitude": 39.9612,
+"longitude": -75.2589
+},
+{
+"name": "Chris' Jazz Cafe",
+"id": 12440,
+"address": "1421 Sansom St",
+"latitude": 39.950155,
+"longitude": -75.1645
+},
+{
+"name": "Academy of Music",
+"id": 219,
+"address": "Broad and Locust Sts",
+"latitude": 39.948216,
+"longitude": -75.164579
+},
+{
+"name": "World Cafe Live",
+"id": 69812,
+"address": "3025 Walnut Street",
+"latitude": 39.9518,
+"longitude": -75.1845
+},
+{
+"name": "Mann Music Center",
+"id": 45991,
+"address": "5201 Parkside Ave",
+"latitude": 39.98149,
+"longitude": -75.2241
+},
+{
+"name": "Electric Factory",
+"id": 3542,
+"address": "421 N 7th St",
+"latitude": 39.958868,
+"longitude": -75.150212
+},
+{
+"name": "Union Transfer",
+"id": 122405,
+"address": "1026 Spring Garden St",
+"latitude": 39.961552,
+"longitude": -75.155319
+},
+{
+"name": "Trocadero",
+"id": 1092,
+"address": "1003 Arch St",
+"latitude": 39.95355,
+"longitude": -75.156323
+},
+{
+"name": "Keswick Theatre",
+"id": 6457,
+"address": "291 Keswick Ave, Glenside",
+"latitude": 40.106318,
+"longitude": -75.147469
+},
+{
+"name": "Susquehanna Bank Center",
+"id": 3242,
+"address": "1 Harbour Blvd, Camden",
+"latitude": 39.94172,
+"longitude": -75.12793
+},
+{
+"name": "Bryn Mawr Gazebo",
+"id": 105822,
+"address": "9 S Bryn Mawr Ave, Bryn Mawr",
+"latitude": 40.02102,
+"longitude": -75.31800
+},
+{
+"name": "Burlap and Bean",
+"id": 67615,
+"address": "204 S Newtown Street Rd, Newtown Square",
+"latitude": 39.9781868,
+"longitude": -75.3976719
+}
+];
