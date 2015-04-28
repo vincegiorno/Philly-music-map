69f4ada7529a087cfc9188d1f290f5b247dd5af6
vinceg <vincegiorno324@gmail.com>
2015-04-27 23:52:37 -0400

    clean up semicolons

--- js/main.js
+++ js/main.js
@@ -25,95 +25,96 @@ var vm = {};
 vm.searchStr = ko.observable("");
 
 vm.Venue = function(index) {
-  self = this;
-  self.id = data[index].id;
-  self.events = ko.observableArray();
-  vm.loadEvents(self.id);
-  self.name = data[index].name;
-  self.address = data[index].address;
-  self.marker = new google.maps.Marker({
-    position: {
-      lat: data[index].latitude,
-      lng: data[index].longitude
-    },
-    title: self.name,
-    icon: data.image,
-    animation: null,
-    map: null
-  });
-  google.maps.event.addListener(self.marker, 'click', view.openEventsWindow(self.events));
-  self.isVisible = ko.computed(function() {
-      if (self.name.indexOf(vm.searchStr)) >= 0) {
-      return true;
-    } else {
-      for (var i = self.events.length - 1; i >= 0; i--) {
-        if (self.events[i].date.indexOf(vm.searchStr) >= 0) {
-          return true;
-        }
-      }
-    } else {
-      for (var i = self.events.length - 1; i >= 0; i--) {
-        for (var j = self.events[i].artists - 1; j >= 0; j--) {
-          if (self.events[i].artists[j].indexOf(vm.searchStr) >= 0) {
+    self = this;
+    self.id = data[index].id;
+    self.events = ko.observableArray();
+    vm.loadEvents(self.id);
+    self.name = data[index].name;
+    self.address = data[index].address;
+    self.marker = new google.maps.Marker({
+      position: {
+        lat: data[index].latitude,
+        lng: data[index].longitude
+      },
+      title: self.name,
+      icon: data.image,
+      animation: null,
+      map: null
+    });
+    google.maps.event.addListener(self.marker, 'click', view.openEventsWindow(self.events));
+    self.isVisible = ko.computed(function() {
+      if (self.name.indexOf(vm.searchStr) >= 0) {
+        return true;
+      } else {
+        for (var i = self.events.length - 1; i >= 0; i--) {
+          if (self.events[i].date.indexOf(vm.searchStr) >= 0) {
             return true;
           }
         }
+      } else {
+        for (i = self.events.length - 1; i >= 0; i--) {
+          for (var j = self.events[i].artists - 1; j >= 0; j--) {
+            if (self.events[i].artists[j].indexOf(vm.searchStr) >= 0) {
+              return true;
+            }
+          }
+        }
+      } else {
+        return false;
       }
-    } else {
-      return false;
-    }
-  });
+    });
+  };
 
-vm.venues = ko.observableArray();
+    vm.venues = ko.observableArray();
 
-/* Function populates venues with venue objects. it is set up to execute
-twice a second because each */
-vm.initialize = function() {
-  var i = data.length - 1;
-  venues[i] = new Venue(data[i]);
-  venues[i].visible = ko.observable(true);
-  vm.getEvents(i, data[i].id);
-  i -= 1;
-  while (i >= 0) {
-    setTimeout(function() {
-      venues[i] = vm.createVenue(i);
+    /* Function populates venues with venue objects. it is set up to execute
+    twice a second because each */
+    vm.initialize = function() {
+      var i = data.length - 1;
+      venues[i] = new Venue(data[i]);
+      venues[i].visible = ko.observable(true);
+      vm.getEvents(i, data[i].id);
       i -= 1;
-    }, 500);
-  }
-}
-
-vm.getEvents = function(index, id) {
-  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    httpRequest = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { // IE
-    try {
-      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-      try {
-        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-      } catch (e) {}
+      while (i >= 0) {
+        setTimeout(function() {
+          venues[i] = vm.createVenue(i);
+          i -= 1;
+        }, 500);
+      }
     }
-  }
 
-  if (!httpRequest) {
-    alert('Data could not be requested from the server.');
-    return false;
-  }
-  httpRequest.onreadystatechange = processEvents(index);
-  url = "http://api.jambase.com/events?venueId=" + id +
-    "&page=0&api_key=73ntgrhffzwqdcaan4empnrd";
-  httpRequest.open('GET', url);
-  httpRequest.send();
-}
+    vm.getEvents = function(index, id) {
+      if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+      } else if (window.ActiveXObject) { // IE
+        try {
+          httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+          try {
+            httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+          } catch (e) {}
+        }
+      }
 
-function processEvents(index) {
-  if (httpRequest.readyState === 4) {
-    if (httpRequest.status === 200) {
-      venues[index].events(JSON.parse(httpRequest.responseText));
-    } else {
-      alert('There was a problem with the request.');
+      if (!httpRequest) {
+        alert('Data could not be requested from the server.');
+        return false;
+      }
+      httpRequest.onreadystatechange = processEvents(index);
+      url = "http://api.jambase.com/events?venueId=" + id +
+        "&page=0&api_key=73ntgrhffzwqdcaan4empnrd";
+      httpRequest.open('GET', url);
+      httpRequest.send();
+    };
+
+    function processEvents(index) {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          venues[index].events(JSON.parse(httpRequest.responseText));
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
     }
-  }
-}
 
-view.format(data.getEvents(self.id));
+    view.format(data.getEvents(self.id));
